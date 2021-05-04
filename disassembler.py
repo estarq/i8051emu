38efc0dbb68268b101cb23fa975a867d6c01eb07
estarq <61628924+estarq@users.noreply.github.com>
2021-05-04 16:42:03 +0200

    Make it easier to read the code

--- disassembler.py
+++ disassembler.py
@@ -1,28 +1,6 @@
 import mcu
 
 
-class IntelHexFile:
-    def __init__(self, filepath):
-        with open(filepath) as file:
-            # [:-1] to skip the trailing EOF Record
-            self._records = [Record(line.rstrip()) for line in list(file)[:-1]]
-
-    def __iter__(self):
-        for record in self._records:
-            yield record
-
-
-class Record:
-    def __init__(self, line):
-        self.first_byte_addr = int(line[3:7], 16)
-        self._data = line[9:-2]
-
-    def __iter__(self):
-        # Every two consecutive hexadecimal digits (ASCII) represent a byte of data
-        for idx in range(0, len(self._data), 2):
-            yield int(self._data[idx:idx + 2], 16)
-
-
 def disassemble(filepath):
     # Disassemble a file having in mind that arguments may not be in the same record as an opcode
     getting_opcode = True
@@ -47,3 +25,25 @@ def disassemble(filepath):
                         yield addr, len(op), op.opcode, op.args, str(op)
                         getting_opcode = True
                         addr += len(op)
+
+
+class IntelHexFile:
+    def __init__(self, filepath):
+        with open(filepath) as file:
+            # [:-1] to skip the trailing EOF Record
+            self._records = [Record(line.rstrip()) for line in list(file)[:-1]]
+
+    def __iter__(self):
+        for record in self._records:
+            yield record
+
+
+class Record:
+    def __init__(self, line):
+        self.first_byte_addr = int(line[3:7], 16)
+        self._data = line[9:-2]
+
+    def __iter__(self):
+        # Every two consecutive hexadecimal digits (ASCII) represent a byte of data
+        for idx in range(0, len(self._data), 2):
+            yield int(self._data[idx:idx + 2], 16)
